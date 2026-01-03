055f4884cd97a0550eba611ac175c951df5e7eee
David Ayobami <davibams@gmail.com>
2026-01-03 13:31:02 +0100

    Add questions array for quiz functionality

---
+++ js/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "High Text Machine Language",
+            "Hyperlinks Text Mark Language",
+            "Home Tool Markup Language"
+        ],
+        answer: 0
+    },
+    {
+        question: "Which language is used for styling web pages?",
+        options: ["HTML", "JQuery", "CSS", "XML"],
+        answer: 2
+    },
+    {
+        question: "Which of the following is not a programming language?",
+        options: ["JavaScript", "Python", "HTML", "PHP"],
+        answer: 2
+    }
+];
